33c806c8f5126cfd2cac2513da89a8566465096f
mood <m.almousalli@gmail.com>
2024-05-22 12:50:53 +0200

    04 / static void output ()

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,27 +8,31 @@ public class App {
 // Cat --> referenztyp (Klasse aus dem das objekt entsteht)
 // cat1 --< referenzvariable (Adresse des objekt im RAM)
 Cat cat1 = new Cat(); //Instanziierung >>
-System.out.println("Blick von aussen: " + cat1); // Adresse desObjekts
+output("Blick von aussen: " + cat1); // Adresse desObjekts
 
 cat1.sayHi();
-System.out.println("-----------------");
+output("-----------------");
 Cat cat2 = new Cat();
 
-System.out.println("Blick von aussen: " + cat2);
+output("Blick von aussen: " + cat2);
 cat2.sayHi();
 
 //System.out.println(new Cat());
 //System.out.println(new Cat());
 
 
-
-
-
-
+output("hi");
 
     }
+// Statische Methode , die in der Klasse ausgeführt wird ...
+  public static void output(String outputStr) {
+System.out.println(outputStr);
 
-   
+   }
+
+
+
+
 
 }
 
