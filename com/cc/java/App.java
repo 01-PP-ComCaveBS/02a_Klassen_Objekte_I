7e7668ad8034e0e48cb2a3c85e23f5ed5d60947e
mood <m.almousalli@gmail.com>
2024-05-22 12:00:02 +0200

    03 /Instanzvariable

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,11 +8,14 @@ public class App {
 // Cat --> referenztyp (Klasse aus dem das objekt entsteht)
 // cat1 --< referenzvariable (Adresse des objekt im RAM)
 Cat cat1 = new Cat(); //Instanziierung >>
-System.out.println(cat1); // Adresse desObjekts
+System.out.println("Blick von aussen: " + cat1); // Adresse desObjekts
 
-
+cat1.sayHi();
+System.out.println("-----------------");
 Cat cat2 = new Cat();
-System.out.println(cat2);
+
+System.out.println("Blick von aussen: " + cat2);
+cat2.sayHi();
 
 //System.out.println(new Cat());
 //System.out.println(new Cat());
