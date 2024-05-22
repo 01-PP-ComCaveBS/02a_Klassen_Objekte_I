466cf5109c30be361c0b8dea707d7df0be3d6cc3
mood <m.almousalli@gmail.com>
2024-05-22 10:47:16 +0200

    02 / Referenzvariablen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,9 +5,20 @@ public class App {
     public static void main(String[] args) {
 
 
-// zwei Objekte erzeugen
-System.out.println(new Cat());
-System.out.println(new Cat());
+// Cat --> referenztyp (Klasse aus dem das objekt entsteht)
+// cat1 --< referenzvariable (Adresse des objekt im RAM)
+Cat cat1 = new Cat(); //Instanziierung >>
+System.out.println(cat1); // Adresse desObjekts
+
+
+Cat cat2 = new Cat();
+System.out.println(cat2);
+
+//System.out.println(new Cat());
+//System.out.println(new Cat());
+
+
+
 
 
 
